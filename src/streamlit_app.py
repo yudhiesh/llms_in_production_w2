77c4f10d5f8dda1809b7c7b0f2d03bd67b1f1a72
Yudhiesh Ravindranath <yudhiesh1997@gmail.com>
2024-02-01 17:35:40 +0800

    add gpt cache

--- src/streamlit_app.py
+++ src/streamlit_app.py
@@ -1,8 +1,13 @@
 import os
+import time
 
 import guardrails as gd
 import openai
 import streamlit as st
+from gptcache import Cache, Config
+from gptcache.adapter.api import get, init_similar_cache, put
+from gptcache.processor.post import nop
+from gptcache.processor.pre import get_prompt
 
 from src.constants import OPENAI_MODEL_ARGUMENTS, PROMPT
 from src.models import ValidSQL
@@ -22,22 +27,57 @@ st.title("SQL Code Generator")
 openai_api_key = st.sidebar.text_input("OpenAI API Key", type="password")
 os.environ["OPENAI_API_KEY"] = openai_api_key
 
-guard = gd.Guard.from_pydantic(output_class=ValidSQL, prompt=PROMPT)
+
+@st.cache_resource
+def get_cache():
+    cache.init(pre_embedding_func=get_prompt)
+    inner_cache = Cache()
+    init_similar_cache(
+        cache_obj=inner_cache, post_func=nop, config=Config(similarity_threshold=0.9)
+    )
+    return inner_cache
 
 
-def generate_response(input_text: str) -> None:
+@st.cache_resource
+def get_guard():
+    guard = gd.Guard.from_pydantic(output_class=ValidSQL, prompt=PROMPT)
+    return guard
+
+
+guard = get_guard()
+cache = get_cache()
+
+
+def generate_response(input_text: str, cache: Cache) -> None:
     try:
-        raw_llm_response, validated_response, reask, validation_passed, error = guard(
-            openai.chat.completions.create,
-            prompt_params={"nl_instruction": input_text},
-            **OPENAI_MODEL_ARGUMENTS,
-        )
-        if error or not validation_passed or not validated_response:
-            st.error(f"Unable to produce an answer due to: {error}")
+        start_time = time.time()
+        cached_result = get(input_text, cache_obj=cache, top_k=1)
+        if not cached_result:
+            (
+                raw_llm_response,
+                validated_response,
+                reask,
+                validation_passed,
+                error,
+            ) = guard(
+                openai.chat.completions.create,
+                prompt_params={"nl_instruction": input_text},
+                **OPENAI_MODEL_ARGUMENTS,
+            )
+            total_time = time.time() - start_time
+            if error or not validation_passed or not validated_response:
+                st.error(f"Unable to produce an answer due to: {error}")
+            else:
+                valid_sql = ValidSQL(**validated_response)
+                generated_sql = valid_sql.generated_sql
+                st.info(generated_sql)
+                st.info(f"That query took: {total_time:.2f}s")
+                put(input_text, generated_sql, cache_obj=cache)
+
         else:
-            valid_sql = ValidSQL(**validated_response)
-            generated_sql = valid_sql.generated_sql
-            st.info(generated_sql)
+            total_time = time.time() - start_time
+            st.info(cached_result[0])
+            st.info(f"That query took: {total_time:.2f}s")
     except Exception as e:
         st.error(f"Error: {e}")
 
@@ -51,4 +91,4 @@ with st.form("my_form"):
     if not openai_api_key.startswith("sk-"):
         st.error("Please enter your OpenAI API key!", icon="⚠️")
     if submitted and openai_api_key.startswith("sk-"):
-        generate_response(text)
+        generate_response(text, cache)

29f77da73ea86623f19f2bda2ccb80349edbbb85
Yudhiesh Ravindranath <yudhiesh1997@gmail.com>
2024-02-13 00:01:48 +0800

    add task 1 base

--- src/cached_resources.py
+++ src/cached_resources.py
@@ -1,42 +1,8 @@
 import guardrails as gd
 import streamlit as st
-from gptcache import Cache
-from gptcache.adapter.api import SearchDistanceEvaluation, init_similar_cache
-from gptcache.embedding import Onnx
-from gptcache.manager import CacheBase, VectorBase, get_data_manager
-from gptcache.processor.post import nop
-from phoenix.trace.openai import OpenAIInstrumentor
 
 from src.models import ValidSQL
 from src.prompt import PROMPT
-
-
-@st.cache_resource
-def instrument() -> None:
-    """
-    Instrument the OpenAI API using Phoenix.
-    """
-    OpenAIInstrumentor().instrument()
-
-
-@st.cache_resource
-def get_cache() -> Cache:
-    """
-    Create a cache using GPTCache.
-    """
-    inner_cache = Cache()
-    onnx = Onnx()
-    data_manager = get_data_manager(
-        CacheBase("sqlite"),
-        VectorBase("faiss", dimension=onnx.dimension),
-    )
-    init_similar_cache(
-        cache_obj=inner_cache,
-        post_func=nop,
-        data_manager=data_manager,
-        evaluation=SearchDistanceEvaluation(),
-    )
-    return inner_cache
 
 
 @st.cache_resource
